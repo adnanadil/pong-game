d489490059f882ef91b97c6c2b598b3be3d73ca6
ADNAN ADIL <adnanadil@ADNANs-MacBook-Pro.local>
2024-09-16 01:22:29 +0300

    Look into socket.io docs and code

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// You can start coding here on M3 max
+const http = require('http')
+const { app } = require('./api')
+
+// To use Socket.io we basically create a new Socket server
+const socket = require("socket.io");
+
+const PORT = 3000
+
+const HTTPserver = http.createServer(app)
+const socketServer = socket(HTTPserver);
+
+const {listen} = require('./socket');
+
+
+
+
+HTTPserver.listen(PORT, () => {
+    console.log(`Server listening to port ${PORT}`)
+})
+
+// To separate it into a folder what we will do is that 
+// we will export the socketServer and feed it into a function 
+// this function will use the socketServer and carry out all the
+// tasks
+
+listen(socketServer)
--- socket.js
+++ socket.js
@@ -1,1 +1,18 @@
 // Note there is no need to have socket.io in a seperate file we can have in the server.js
+
+let playerCount = 0;
+
+function listen(socketServer){
+
+    // Here I have to make use of rooms and namespaces. 
+
+    socketServer.on('connection', (socket) => {
+        console.log(`Device Connected with id ${socket.id}`)
+    })
+
+    
+}
+
+module.exports = {
+    listen
+}
