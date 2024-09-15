6f2a53ad7c8440e2020f405d0788b7e1ccf6aae0
ADNAN ADIL <adnanadil@ADNANs-MacBook-Pro.local>
2024-09-16 01:26:47 +0300

    ready to start at office

--- api.js
+++ api.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const path = require('path')
 
 const app = express()
 
-app.get('/', (req,res)=> {
-    console.log("Hello World")
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', express.static('index.html'));
+
 
 module.exports = {
     app
